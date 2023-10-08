53d996fb3b736793bf04a755887a9c6a2534568e
Slava (Sviataslau Salamennik) <sviataslau.salamennik@mavenmachines.com>
2023-10-08 14:55:43 +0400

    fix eslint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import asyncHandler from 'express-async-handler'
 import { handleMessage } from '../routeHandlers/handleMessage';
 import validateMessageMiddleware from '../middlewares/validateMessageMiddleware';
 
 const router = Router();
 
-router.post('/message', validateMessageMiddleware, handleMessage);
+router.post('/message', validateMessageMiddleware, asyncHandler(handleMessage));
 
 export default router;
