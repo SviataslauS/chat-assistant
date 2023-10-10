4f3559319e0a28117b9be1412c2a9fb85c217066
Slava (Sviataslau Salamennik) <sviataslau.salamennik@mavenmachines.com>
2023-10-10 14:04:01 +0400

    change tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.test' });
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
--- src/routeHandlers/handleMessage.test.ts
+++ src/routeHandlers/handleMessage.test.ts
@@ -1,56 +1,41 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import MockAdapter from 'axios-mock-adapter';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { handleMessage } from './handleMessage';
 import ChatMessage from '../models/ChatMessage';
-import envConfig from '../envConfig';
 import logger from '../logger';
-import openai from '../openai';
 
 const axiosMock = new MockAdapter(axios);
 
 jest.mock('../models/ChatMessage', () => ({
-  findAll: jest.fn(),
   create: jest.fn(),
+  findAll: jest.fn(),
 }));
 
-type ReturnJestFnType = ReturnType<typeof jest.fn>
-jest.mock('../logger', () => {
-  return {
-    __esModule: true,
-    default: {
-      error: jest.fn(),
-      info: jest.fn(),
-    },
-  };
-});
+jest.mock('../logger', () => ({
+  error: jest.fn(),
+  info: jest.fn(),
+}));
 
-jest.mock('../openai', () => {
-  return {
-    __esModule: true,
-    default: {
-      completions: jest.fn(),
-    },
-  };
-});
-function getOpenaiCompletions (): ReturnJestFnType {
-  return openai.completions as unknown as ReturnJestFnType;
-}
-const openaiMock = getOpenaiCompletions();
+const mockedChatMessageCreate = ChatMessage.create as jest.MockedFunction<typeof ChatMessage.create>;
+const mockedChatMessageFindAll = ChatMessage.findAll as jest.MockedFunction<typeof ChatMessage.findAll>;
+const mockedLoggerError = logger.error as jest.MockedFunction<typeof logger.error>;
+const mockedLoggerInfo = logger.info as jest.MockedFunction<typeof logger.info>;
 
 describe('handleMessage', () => {
-  afterEach(() => {
+  beforeEach(() => {
     axiosMock.reset();
-    jest.clearAllMocks();
+    mockedChatMessageCreate.mockReset();
+    mockedChatMessageFindAll.mockReset();
+    mockedLoggerError.mockReset();
+    mockedLoggerInfo.mockReset();
   });
 
-  it('should handle a valid request and send a response to the chat service', async () => {
-    axiosMock.onPost(`${envConfig.CHAT_SERVICE_API}/send`).reply(200);
-    // @ts-expect-error
-    ChatMessage.findAll.mockResolvedValue([]);
-    // @ts-expect-error
-    ChatMessage.create.mockResolvedValue({});
+  it('should handle a valid request', async () => {
+    axiosMock.onPost('https://server.com/send').reply(200);
+
+    mockedChatMessageCreate.mockResolvedValue(null);
+    mockedChatMessageFindAll.mockResolvedValue([]);
 
     const req = {
       body: {
@@ -63,183 +48,82 @@ describe('handleMessage', () => {
       sendStatus: jest.fn(),
     };
 
-    // @ts-expect-error
-    await handleMessage(req, res);
+    await handleMessage(req as any, res as any);
 
     expect(res.sendStatus).toHaveBeenCalledWith(200);
-    expect(ChatMessage.findAll).toHaveBeenCalledWith({
+    expect(mockedChatMessageCreate).toHaveBeenCalledWith({
+      message: 'Test message',
+      userId: '123',
+    });
+    expect(mockedChatMessageFindAll).toHaveBeenCalledWith({
       where: { userId: '123' },
       attributes: ['message'],
     });
-    expect(axiosMock.history.post.length).toBe(1);
-    expect(axiosMock.history.post[0].data).toEqual({
-      side: 'server',
-      message: expect.any(String),
-      userId: '123',
-    });
-  });
-  it('should handle a valid request and send a message to the chat service', async () => {
-    axiosMock.onPost(`${envConfig.CHAT_SERVICE_API}/send`).reply(200);
-
-    const req = {
-      body: {
-        message: 'Test message',
-        userId: '123',
-      },
-    };
-
-    const res = {
-      sendStatus: jest.fn(),
-    };
-
-    const createSpy = jest.spyOn(ChatMessage, 'create').mockResolvedValueOnce({});
-
-    // @ts-expect-error:
-    await handleMessage(req, res);
-
-    expect(res.sendStatus).toHaveBeenCalledWith(200);
-    expect(createSpy).toHaveBeenCalledWith({
-      message: 'Test message',
-      userId: '123',
-    });
-    expect(logger.error).not.toHaveBeenCalled();
-  });
-
-  it('should handle an error when saving the client message', async () => {
-    const req = {
-      body: {
-        message: 'Test message',
-        userId: '123',
-      },
-    };
-
-    const res = {
-      sendStatus: jest.fn(),
-    };
-
-    const createSpy = jest
-      .spyOn(ChatMessage, 'create')
-      .mockRejectedValueOnce(new Error('DB Error'));
-
-    // @ts-expect-error:
-    await handleMessage(req, res);
-
-    expect(res.sendStatus).toHaveBeenCalledWith(200);
-    expect(createSpy).toHaveBeenCalledWith({
-      message: 'Test message',
-      userId: '123',
-    });
-    expect(logger.error).toHaveBeenCalledWith('Save client message error:', 'DB Error');
-  });
-
-  it('should handle a valid request but receive an empty AI response', async () => {
-    axiosMock.onPost(`${envConfig.CHAT_SERVICE_API}/send`).reply(200);
-
-    const req = {
-      body: {
-        message: 'Test message',
-        userId: '123',
-      },
-    } as unknown as Response;;
-
-    const res = {
-      sendStatus: jest.fn(),
-    };
-
-    const createSpy = jest.spyOn(ChatMessage, 'create').mockResolvedValueOnce({});
-    openaiMock.mockResolvedValueOnce({ choices: [] } as unknown as never);
-
-    try {
-      // @ts-expect-error:
-      await handleMessage(req, res);
-    } catch (error) {
-      
-    }
-
-    expect(res.sendStatus).toHaveBeenCalledWith(200);
-    expect(createSpy).toHaveBeenCalledWith({
-      message: 'Test message',
-      userId: '123',
-    });
-    // expect(logger.info).toHaveBeenCalledWith(
-    //   expect.stringContaining('Empty AI response. Request body:'),
-    // );
-    expect(openaiMock).toHaveBeenCalled();
-  });
-
-  it('should handle a request and send an AI response to the chat service', async () => {
-    axiosMock.onPost(`${envConfig.CHAT_SERVICE_API}/send`).reply(200);
-
-    const req = {
-      body: {
-        message: 'Test message',
-        userId: '123',
-      },
-    };
-
-    const res = {
-      sendStatus: jest.fn(),
-    };
-
-    const createSpy = jest.spyOn(ChatMessage, 'create').mockResolvedValueOnce({});
-    const openaiMock = jest
-      .spyOn(require('../openai'), 'completions')
-      .mockResolvedValueOnce({ choices: [{ text: 'AI reply' }] });
-
-    // @ts-expect-error:
-    await handleMessage(req, res);
-
-    expect(res.sendStatus).toHaveBeenCalledWith(200);
-    expect(createSpy).toHaveBeenCalledWith({
-      message: 'Test message',
-      userId: '123',
-    });
-    expect(logger.info).not.toHaveBeenCalledWith(
-      expect.stringContaining('Empty AI response. Request body:'),
-    );
-    expect(openaiMock).toHaveBeenCalledWith(
-      {
-        model: 'text-davinci-002',
-        prompt: 'Test message',
-      },
-      expect.anything(),
-    );
   });
 
   it('should handle an error when sending a request to the chat service', async () => {
-    axiosMock.onPost(`${envConfig.CHAT_SERVICE_API}/send`).reply(500);
-    const createSpy = jest.spyOn(ChatMessage, 'create').mockResolvedValueOnce({});
-    const openaiMock = jest
-      .spyOn(require('../openai'), 'completions')
-      .mockResolvedValueOnce({ choices: [{ text: 'AI reply' }] });
+    axiosMock.onPost('https://server.com/send').reply(500);
+
+    mockedChatMessageCreate.mockResolvedValue(null);
+    mockedChatMessageFindAll.mockResolvedValue([]);
 
     const req = {
       body: {
         message: 'Test message',
         userId: '123',
       },
-    } as unknown as Request;
+    };
 
     const res = {
       sendStatus: jest.fn(),
     };
-  
-    // @ts-expect-error:
-    await handleMessage(req, res);
+
+    await handleMessage(req as any, res as any);
 
     expect(res.sendStatus).toHaveBeenCalledWith(200);
-    expect(createSpy).toHaveBeenCalledWith({
+    expect(mockedChatMessageCreate).toHaveBeenCalledWith({
       message: 'Test message',
       userId: '123',
     });
-    expect(logger.error).toHaveBeenCalledWith('Chat service error:', expect.any(String));
-    expect(openaiMock).toHaveBeenCalledWith(
-      {
-        model: 'text-davinci-002',
-        prompt: 'Test message',
-      },
-      expect.anything(),
-    );
+    expect(mockedChatMessageFindAll).toHaveBeenCalledWith({
+      where: { userId: '123' },
+      attributes: ['message'],
+    });
+    expect(mockedLoggerError).toHaveBeenCalledWith(expect.any(String));
   });
 
+  it('should handle a valid request and execute setTimeout', async () => {
+    axiosMock.onPost('https://server.com/send').reply(200);
+
+    mockedChatMessageCreate.mockResolvedValue(null);
+    mockedChatMessageFindAll.mockResolvedValue([]);
+
+    const req = {
+      body: {
+        message: 'Test message',
+        userId: '123',
+      },
+    };
+
+    const res = {
+      sendStatus: jest.fn(),
+    };
+
+    const originalSetTimeout = setTimeout;
+    (global as any).setTimeout = jest.fn((callback: () => void) => callback);
+
+    await handleMessage(req as any, res as any);
+
+    expect(res.sendStatus).toHaveBeenCalledWith(200);
+    expect(mockedChatMessageCreate).toHaveBeenCalledWith({
+      message: 'Test message',
+      userId: '123',
+    });
+    expect(mockedChatMessageFindAll).toHaveBeenCalledWith({
+      where: { userId: '123' },
+      attributes: ['message'],
+    });
+
+    (global as any).setTimeout = originalSetTimeout;
+  });
 });
